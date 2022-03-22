29ccb1fb5d8f624ae65dd6c9de1c241c92a14938
Eder Matos <ederfmatos@gmail.com>
2022-03-22 10:58:58 -0300

    test: ensure AccountMongoRepository returns as account on success

---
+++ src/infra/database/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,33 @@
+import { MongoHelper } from '../helpers/mongo.helper'
+import { AccountMongoRepository } from './account.repository'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
