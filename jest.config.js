431c666968b3aa831eb794369b90ac0637fe8d57
Eder Matos <ederfmatos@gmail.com>
2022-03-22 13:16:06 -0300

    chore: remove main layer from jest coverage

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,12 @@ module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/*.protocol.ts', '!<rootDir>/src/**/index.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.protocol.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   transform: {
     '.+\\.ts$': 'ts-jest'
