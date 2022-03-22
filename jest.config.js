afaec6eed752aafc2b772d6898bdf7363ffab932
Eder Matos <ederfmatos@gmail.com>
2022-03-22 00:29:41 -0300

    chore: ignore files to coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/*.protocol.ts', '!<rootDir>/src/**/index.ts'],
   coverageDirectory: 'coverage',
   transform: {
     '.+\\.ts$': 'ts-jest'
