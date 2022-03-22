72b52700d83b5246c8c5cf54dc42364c31ec1b16
Eder Matos <ederfmatos@gmail.com>
2022-03-22 00:10:55 -0300

    chore: remove jest coverage provider

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   collectCoverage: false,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
