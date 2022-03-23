17d6ba2fa3009fec13cf66d477064605bd05c7cb
Eder Matos <ederfmatos@gmail.com>
2022-03-23 17:55:31 -0300

    test: ensure ValidationComposite not return if none validation succeds

--- src/presentation/helpers/validations/validation-composite.spec.ts
+++ src/presentation/helpers/validations/validation-composite.spec.ts
@@ -40,4 +40,10 @@ describe('Validation Composite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error('any_message'))
   })
+
+  test('should not return if none validation succeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
