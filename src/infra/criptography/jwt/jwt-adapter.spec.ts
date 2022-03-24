6edcd9081b43ffd842009aaa38c20975827074b1
Eder Matos <ederfmatos@gmail.com>
2022-03-24 15:42:58 -0300

    test: ensure JwtAdapter returns a token on sign success

--- src/infra/criptography/jwt/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt/jwt-adapter.spec.ts
@@ -1,8 +1,10 @@
-import JsonWebToken from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt.adapter'
 
 jest.mock('jsonwebtoken', () => ({
-  sign: () => 'any_value_enprypted'
+  async sign (): Promise<string> {
+    return 'any_value_encrypted'
+  }
 }))
 
 function makeSut (): JwtAdapter {
@@ -12,15 +14,23 @@ function makeSut (): JwtAdapter {
 describe('Jwt Adapter', () => {
   test('should call sign with correct values', async () => {
     const sut = makeSut()
-    const signSpy = jest.spyOn(JsonWebToken, 'sign')
+    const signSpy = jest.spyOn(jwt, 'sign')
 
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenNthCalledWith(1, { id: 'any_id' }, 'secret')
   })
 
+  test('should return a token on sign success', async () => {
+    const sut = makeSut()
+
+    const token = await sut.encrypt('any_id')
+    expect(token).toBe('any_value_encrypted')
+  })
+
   test('should call sign with correct values', async () => {
     const sut = makeSut()
-    jest.spyOn(JsonWebToken, 'sign').mockImplementation(() => {
+
+    jest.spyOn(jwt, 'sign').mockImplementation(() => {
       throw new Error('any-message')
     })
 
