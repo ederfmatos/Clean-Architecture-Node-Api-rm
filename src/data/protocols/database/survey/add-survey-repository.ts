20fb43b90367f9aabb419ab2fd9e4284b77c1079
Eder Matos <ederfmatos@gmail.com>
2022-04-01 18:52:20 -0300

    fix: fix wrong import

--- src/data/protocols/database/survey/add-survey-repository.ts
+++ src/data/protocols/database/survey/add-survey-repository.ts
@@ -1,4 +1,4 @@
-import { AddSurveyModel } from '../../../../presentation/protocols'
+import { AddSurveyModel } from '../../../usecases/add-survey/db-add-survey.protocol'
 
 export interface AddSurveyRepository {
   add: (addSurveyModel: AddSurveyModel) => Promise<void>
