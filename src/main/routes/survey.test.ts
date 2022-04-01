4bed5bb61c8f4a040b43c1eb76fdf1861acf3a61
Eder Matos <ederfmatos@gmail.com>
2022-04-01 19:59:45 -0300

    test: ensure add survey route returns 403 if account role if not admin

--- src/main/routes/survey.test.ts
+++ src/main/routes/survey.test.ts
@@ -77,5 +77,38 @@ describe('Survey Route', () => {
         })
         .expect(204)
     })
+
+    test('should return 403 on account role is not ADMIN', async () => {
+      const { insertedId } = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+
+      const accountId = insertedId.toString()
+      const accessToken = sign({ id: accountId }, env.jwtSecret)
+
+      await accountCollection.updateOne(
+        { _id: new ObjectId(accountId) },
+        { $set: { accessToken } }
+      )
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [
+            {
+              answer: 'any_answer',
+              image: 'any_image'
+            },
+            {
+              answer: 'another_answer'
+            }
+          ]
+        })
+        .expect(403)
+    })
   })
 })
