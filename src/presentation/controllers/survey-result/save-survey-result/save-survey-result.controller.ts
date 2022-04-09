1ea582fe953f189ec0969f52a1d20f25996e232c
Eder Matos <ederfmatos@gmail.com>
2022-04-09 19:32:02 -0300

    feat: ensure SaveSurveyResultController returns 403 if invalid surveyId is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result.controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, HttpRequest, HttpResponse } from '../../auth/login/login.protocol'
-import { LoadSurveyById } from './save-survey-result.protocol'
+import { LoadSurveyById, Controller, HttpRequest, HttpResponse, InvalidParamError } from './save-survey-result.protocol'
+import { forbidden } from '@/presentation/helpers/http/http.helper'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { surveyId } = httpRequest.params
-    await this.loadSurveyById.loadById(surveyId)
+    const survey = await this.loadSurveyById.loadById(surveyId)
+    if (!survey) {
+      return forbidden(new InvalidParamError('surveyId'))
+    }
+
     return null
   }
 }
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result.protocol.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result.protocol.ts
@@ -1,2 +1,4 @@
 export * from '@/domain/usecases/survey/load-survey-by-id.usecase'
 export * from '@/domain/models/survey.model'
+export * from '@/presentation/protocols'
+export * from '@/presentation/errors'
