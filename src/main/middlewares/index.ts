ab3ffd2f7602b957c8f9ad850385a5272a98d769
Eder Matos <ederfmatos@gmail.com>
2022-04-11 07:10:47 -0300

    feat: add no cache middleware

--- src/main/middlewares/index.ts
+++ src/main/middlewares/index.ts
@@ -1,3 +1,4 @@
 export * from './body-parser/body-parser.middleware'
 export * from './content-type/content-type.middleware'
 export * from './cors/cors.middleware'
+export * from './no-cache/no-cache'
