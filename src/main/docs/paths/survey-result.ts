98807e7f4068f21a723900648ad989b164bbd27e
Eder Matos <ederfmatos@gmail.com>
2022-04-15 14:53:16 -0300

    docs: add load survey result api docs

--- src/main/docs/paths/survey-result.ts
+++ src/main/docs/paths/survey-result.ts
@@ -1,4 +1,44 @@
 export const surveyResultPath = {
+  get: {
+    security: [{ ApiKeyAuth: [] }],
+    tags: ['Enquetes'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description: 'Essa rota só pode ser consultada por **usuários authenticados**',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string'
+        }
+      }
+    ],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
   put: {
     security: [{ ApiKeyAuth: [] }],
     tags: ['Enquetes'],
