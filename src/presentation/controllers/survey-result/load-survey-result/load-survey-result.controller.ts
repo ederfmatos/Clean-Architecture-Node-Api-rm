3c0a272985e4083ac5efce836fb1803288011032
Eder Matos <ederfmatos@gmail.com>
2022-04-15 14:16:34 -0300

    feat: ensure LoadSurveyResult Controller returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result.controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result.controller.ts
@@ -1,18 +1,22 @@
-import { forbidden } from '@/presentation/helpers/http/http.helper'
-import { InvalidParamError } from '@/presentation/errors/invalid-param-error.error'
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './load-survey-result.protocol'
+import { forbidden, serverError } from '@/presentation/helpers/http/http.helper'
+import { InvalidParamError } from '@/presentation/errors/invalid-param-error.error'
 
 export class LoadSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { surveyId } = httpRequest.params
+    try {
+      const { surveyId } = httpRequest.params
 
-    const survey = await this.loadSurveyById.loadById(surveyId)
-    if (!survey) {
-      return forbidden(new InvalidParamError('surveyId'))
+      const survey = await this.loadSurveyById.loadById(surveyId)
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'))
+      }
+
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-
-    return null
   }
 }
--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result.spec.ts
@@ -1,4 +1,4 @@
-import { forbidden } from '@/presentation/helpers/http/http.helper'
+import { forbidden, serverError } from '@/presentation/helpers/http/http.helper'
 import { mockLoadSurveyById } from '@/presentation/test'
 import { InvalidParamError } from '@/presentation/errors/invalid-param-error.error'
 import { LoadSurveyResultController } from './load-survey-result.controller'
@@ -40,5 +40,12 @@ describe('LoadSurveyResult Controller', () => {
     jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(null)
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
+  })
+
+  test('should returns 500 if LoadSurveyById throws', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockRejectedValueOnce(new Error('any message'))
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(new Error('any message')))
   })
 })
