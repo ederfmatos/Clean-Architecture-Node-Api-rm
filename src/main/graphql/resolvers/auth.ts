d395f483dc83a1e78fadbd0bdd3e9d9263ffbdef
Eder Matos <ederfmatos@gmail.com>
2022-04-20 22:46:57 -0300

    feat: add signUp mutation

--- src/main/graphql/resolvers/auth.ts
+++ src/main/graphql/resolvers/auth.ts
@@ -1,10 +1,14 @@
 import { handleApolloServerResolver } from '@/main/adapters'
-import { makeLoginController } from '@/main/factories'
+import { makeLoginController, makeSignUpController } from '@/main/factories'
 
 const loginController = makeLoginController()
+const signUpController = makeSignUpController()
 
 export default {
   Query: {
     login: async (parent: any, args: any) => handleApolloServerResolver(loginController, args)
+  },
+  Mutation: {
+    signUp: async (parent: any, args: any) => handleApolloServerResolver(signUpController, args)
   }
 }
